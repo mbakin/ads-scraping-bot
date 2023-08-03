cb127afa4c3ff36d343e9c4da222f230810a0fcb
Berk Akin <berk@thecodebase.io>
2023-08-03 11:32:03 +0300

    extend command args with estate

--- index.js
+++ index.js
@@ -7,49 +7,87 @@ import { fileOperations } from "./src/file.js";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-let dir = path.join(__dirname, "db", "cars", "summary");
-
-if (!fs.existsSync(dir)) {
-  fs.mkdirSync(dir, { recursive: true });
-}
-
 let main = {
   async init() {
-    for (const arg of process.argv) {
-      if (arg.startsWith("car=")) {
-        const model = arg.substr(4);
+    const type = process.argv.find((arg) => arg.startsWith("type=")).substr(5);
 
-        console.log("starting car: " + model);
+    switch (type) {
+      case "car":
+        let carDir = path.join(__dirname, "db", "summary", "car");
 
-        const serviceResult = await cars.getByFilters(
-          filters[`get${model}filter`]()
-        );
-        console.log(`found ${serviceResult.length} ${model}`);
+        if (!fs.existsSync(carDir)) {
+          fs.mkdirSync(carDir, { recursive: true });
+        }
+        for (const arg of process.argv) {
+          if (arg.startsWith("car=")) {
+            const model = arg.substr(4);
 
-        const fileName = `db/cars/summary/${model}.json`;
+            console.log("starting car: " + model);
 
-        const dbData = fileOperations.readFile(fileName);
+            const serviceResult = await cars.getByFilters(
+              filters[`get${model}filter`]()
+            );
+            console.log(`found ${serviceResult.length} ${model}`);
 
-        const updatedDbData = await cars.getUpdatedDbData(
-          dbData,
-          serviceResult
-        );
+            const fileName = `db/summary/car/${model}.json`;
 
-        fileOperations.writeToFile(fileName, updatedDbData);
+            const dbData = fileOperations.readFile(fileName);
 
-        let changes = updatedDbData
-          .filter(
-            (x) =>
-              Array.isArray(x.prices) && x.prices.length > x.notifyFlagByPrices
-          )
-          .sort(
-            (a, b) =>
-              a.prices[0] -
-              a.prices[a.prices.length - 1] -
-              (b.prices[0] - b.prices[b.prices.length - 1])
-          );
-        console.log(changes);
-      }
+            const updatedDbData = await cars.getUpdatedDbData(
+              dbData,
+              serviceResult
+            );
+
+            fileOperations.writeToFile(fileName, updatedDbData);
+
+            let changes = updatedDbData
+              .filter(
+                (x) =>
+                  Array.isArray(x.prices) &&
+                  x.prices.length > x.notifyFlagByPrices
+              )
+              .sort(
+                (a, b) =>
+                  a.prices[0] -
+                  a.prices[a.prices.length - 1] -
+                  (b.prices[0] - b.prices[b.prices.length - 1])
+              );
+            console.log(changes);
+          }
+        }
+        break;
+      case "estate":
+        let estateDir = path.join(__dirname, "db", "summary", "estate");
+
+        if (!fs.existsSync(estateDir)) {
+          fs.mkdirSync(estateDir, { recursive: true });
+        }
+        for (const arg of process.argv) {
+          if (arg.startsWith("estate=")) {
+            const estateFilter = arg.substr(7);
+
+            console.log("starting estate: " + estateFilter);
+
+            const serviceResult = await estates.getByFilters(
+              filters[`get${estateFilter}filter`]()
+            );
+            console.log(`found ${serviceResult.length} estates`);
+
+            const fileName = `db/summary/estate/${estateFilter}.json`;
+
+            const dbData = fileOperations.readFile(fileName);
+
+            const updatedDbData = await estates.getUpdatedDbData(
+              dbData,
+              serviceResult
+            );
+
+            fileOperations.writeToFile(fileName, updatedDbData);
+          }
+        }
+        break;
+      default:
+        console.log("Invalid type. Please enter a valid type (car or estate).");
     }
   },
 };
